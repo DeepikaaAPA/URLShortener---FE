5444a7e8b1d5d6027601cd335b9a8348001f12d7
DeepikaaAPA <deepikaarjun@gmail.com>
2024-09-13 18:34:50 +0530

    auth header

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,15 +1,26 @@
 import axios from "axios";
 import { baseURL } from "../config/config";
-const token = localStorage.getItem("token") || "";
-console.log(token);
+// const token = localStorage.getItem("token") || "";
+// console.log("token :", token);
 // define the axios instance
 const instance = axios.create({
   baseURL,
   timeout: 20000,
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${token}`,
   },
   // withCredentials: true,
 });
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 export default instance;
